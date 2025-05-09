7484ffb025a662843bd5ae3a41367e5a9ed84d55
C. Todd Lombardo <1851211+iamctodd@users.noreply.github.com>
2025-05-09 01:32:45 +0000

    initial commit

---
+++ networking-recommendations-app/src/App.js
@@ -0,0 +1,11 @@
+import NetworkingRecommendations from './components/NetworkingRecommendations';
+
+function App() {
+  return (
+    <div className="App">
+      <NetworkingRecommendations />
+    </div>
+  );
+}
+
+export default App;
